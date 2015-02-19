d3d863c91009e77bbe31578fa2ddd1d77adcb1f4
Nicholas Schmidt <nick.schmidt3@gmail.com>
2015-02-19 14:07:04 -0900

    Added file i/o function with error handling
    Added file i/o test case

--- fwrater.py
+++ fwrater.py
@@ -13,6 +13,7 @@ from ipaddress import ip_network
 
 #Functions go here
 
+###LIST INTERPRETERS GO HERE###
 #Process IPTables Chain
 # takes a string, preferably multiline, but you want the entire chain.
 #Returns a list, including the action, protocol, src/dest, score
@@ -63,7 +64,7 @@ def processIPTables (IPTables_input):
         return 0
     return 1
 
-
+###UTILITY FUNCTIONS GO HERE###
 #Score Function
 #Takes 2 ip networks, a protocol ID, protocol port (if applicable, if not 0) and a modifier (if applicable, if not 1)
 def score_entry (srcIP, destIP, proto, scoreMOD):
@@ -83,7 +84,23 @@ def print_2d_list (list_2d):
         print(list_2d[i])
 
 
-#Main goes here
+###IO GOES HERE###
+
+
+#Open a file <file_name> if it exists. Return file as a string
+def file_read (filename_to_read):
+    try:
+        file_return_io = open(filename_to_read,"r")
+        print(file_return_io)
+        file_return_list = file_return_io.readlines()
+        file_return_io.close()
+        return file_return_list
+    except OSError:
+        print("File not found!")
+
+###MAIN FUNCTIONALITY GOES HERE###
+file_read("a")
+file_read("fwrater.py")
 print_2d_list(processIPTables("Chain input_ext (1 references)\n" + \
         "target     prot opt source               destination\n" + \
         "DROP       all  --  anywhere             anywhere             PKTTYPE = broadcast\n" + \
